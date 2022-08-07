0abf08a3580577e3dd9c5c8afa373bc94e3d2bd5
matheusguermandi <matheus_guermandi@hotmail.com>
2022-08-07 15:51:04 +0100

    chore: add validation tests to genre entity

--- domain/src/test/java/com/fullcycle/admin/catalogo/domain/genre/GenreTest.java
+++ domain/src/test/java/com/fullcycle/admin/catalogo/domain/genre/GenreTest.java
@@ -1,5 +1,7 @@
 package com.fullcycle.admin.catalogo.domain.genre;
 
+import com.fullcycle.admin.catalogo.domain.exceptions.DomainException;
+import com.fullcycle.admin.catalogo.domain.validation.handler.ThrowsValidationHandler;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -23,4 +25,59 @@ public class GenreTest {
         Assertions.assertNull(actualGenre.getDeletedAt());
     }
 
+    @Test
+    public void givenInvalidNullName_whenCallNewGenreAndValidate_shouldReceiveAError() {
+        final String expectedName = null;
+        final var expectedIsActive = true;
+        final var expectedErrorCount = 1;
+        final var expectedErrorMessage = "'name' should not be null";
+
+        final var actualGenre = Genre.newGenre(expectedName, expectedIsActive);
+
+        final var actualException = Assertions.assertThrows(DomainException.class, () -> {
+            actualGenre.validate(new ThrowsValidationHandler());
+        });
+
+        Assertions.assertEquals(expectedErrorCount, actualException.getErrors().size());
+        Assertions.assertEquals(expectedErrorMessage, actualException.getErrors().get(0).message());
+    }
+
+    @Test
+    public void givenInvalidEmptyName_whenCallNewGenreAndValidate_shouldReceiveAError() {
+        final var expectedName = " ";
+        final var expectedIsActive = true;
+        final var expectedErrorCount = 1;
+        final var expectedErrorMessage = "'name' should not be empty";
+
+        final var actualGenre = Genre.newGenre(expectedName, expectedIsActive);
+
+        final var actualException = Assertions.assertThrows(DomainException.class, () -> {
+            actualGenre.validate(new ThrowsValidationHandler());
+        });
+
+        Assertions.assertEquals(expectedErrorCount, actualException.getErrors().size());
+        Assertions.assertEquals(expectedErrorMessage, actualException.getErrors().get(0).message());
+    }
+
+    @Test
+    public void givenInvalidNameWithLengthGreaterThan255_whenCallNewGenreAndValidate_shouldReceiveAError() {
+        final var expectedName = """
+                Gostaria de enfatizar que o consenso sobre a necessidade de qualificação auxilia a preparação e a
+                composição das posturas dos órgãos dirigentes com relação às suas atribuições.
+                Do mesmo modo, a estrutura atual da organização apresenta tendências no sentido de aprovar a
+                manutenção das novas proposições.
+                """;
+        final var expectedIsActive = true;
+        final var expectedErrorCount = 1;
+        final var expectedErrorMessage = "'name' must be between 1 and 255 characters";
+
+        final var actualGenre = Genre.newGenre(expectedName, expectedIsActive);
+
+        final var actualException = Assertions.assertThrows(DomainException.class, () -> {
+            actualGenre.validate(new ThrowsValidationHandler());
+        });
+
+        Assertions.assertEquals(expectedErrorCount, actualException.getErrors().size());
+        Assertions.assertEquals(expectedErrorMessage, actualException.getErrors().get(0).message());
+    }
 }
