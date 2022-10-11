c23246a1464d147e314f51472694562af7e83d8d
matheusguermandi <matheus_guermandi@hotmail.com>
2022-10-11 16:19:02 +0100

    chore: add e2e test - as admin should be able to sort all members by name/desc

--- infrastructure/src/test/java/com/fullcycle/admin/catalogo/e2e/castmember/CastMemberE2ETest.java
+++ infrastructure/src/test/java/com/fullcycle/admin/catalogo/e2e/castmember/CastMemberE2ETest.java
@@ -140,4 +140,23 @@ public class CastMemberE2ETest implements MockDsl {
                 .andExpect(jsonPath("$.items[0].name", equalTo("Vin Diesel")));
     }
 
+    @Test
+    public void asACatalogAdminIShouldBeAbleToSortAllMembersByNameDesc() throws Exception {
+        Assertions.assertTrue(MYSQL_CONTAINER.isRunning());
+        Assertions.assertEquals(0, castMemberRepository.count());
+
+        givenACastMember("Vin Diesel", CastMemberType.ACTOR);
+        givenACastMember("Quentin Tarantino", CastMemberType.DIRECTOR);
+        givenACastMember("Jason Momoa", CastMemberType.ACTOR);
+
+        listCastMembers(0, 3, "", "name", "desc")
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.current_page", equalTo(0)))
+                .andExpect(jsonPath("$.per_page", equalTo(3)))
+                .andExpect(jsonPath("$.total", equalTo(3)))
+                .andExpect(jsonPath("$.items", hasSize(3)))
+                .andExpect(jsonPath("$.items[0].name", equalTo("Vin Diesel")))
+                .andExpect(jsonPath("$.items[1].name", equalTo("Quentin Tarantino")))
+                .andExpect(jsonPath("$.items[2].name", equalTo("Jason Momoa")));
+    }
 }
