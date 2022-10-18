70de0b28f159c40e0459f817db22794072ccec43
matheusguermandi <matheus_guermandi@hotmail.com>
2022-10-18 20:59:01 +0100

    chore: add e2e test - as admin should be able to see a treated error by deleting a found cast member with invalid values

--- infrastructure/src/test/java/com/fullcycle/admin/catalogo/e2e/castmember/CastMemberE2ETest.java
+++ infrastructure/src/test/java/com/fullcycle/admin/catalogo/e2e/castmember/CastMemberE2ETest.java
@@ -253,5 +253,19 @@ public class CastMemberE2ETest implements MockDsl {
         Assertions.assertFalse(castMemberRepository.existsById(actualId.getValue()));
     }
 
+    @Test
+    public void asACatalogAdminIShouldBeAbleToDeleteACastMemberWithInvalidIdentifier() throws Exception {
+        Assertions.assertTrue(MYSQL_CONTAINER.isRunning());
+        Assertions.assertEquals(0, castMemberRepository.count());
 
+        givenACastMember(Fixture.name(), Fixture.CastMember.type());
+        givenACastMember(Fixture.name(), Fixture.CastMember.type());
+
+        Assertions.assertEquals(2, castMemberRepository.count());
+
+        deleteACastMember(CastMemberID.from("123"))
+                .andExpect(status().isNoContent());
+
+        Assertions.assertEquals(2, castMemberRepository.count());
+    }
 }
