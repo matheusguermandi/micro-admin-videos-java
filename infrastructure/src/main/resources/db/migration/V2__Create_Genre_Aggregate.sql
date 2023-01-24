083bdeefe01b98a4eb9a2ce7a6e534e9b613371d
matheusguermandi <matheus_guermandi@hotmail.com>
2023-01-24 10:34:31 +0000

    chore: update genre aggregate migration with new category table name

--- infrastructure/src/main/resources/db/migration/V2__Create_Genre_Aggregate.sql
+++ infrastructure/src/main/resources/db/migration/V2__Create_Genre_Aggregate.sql
@@ -1,4 +1,3 @@
-
 CREATE TABLE genres (
     id CHAR(32) NOT NULL PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
@@ -13,5 +12,5 @@ CREATE TABLE genres_categories (
     category_id CHAR(32) NOT NULL,
     CONSTRAINT idx_genre_category UNIQUE (genre_id, category_id),
     CONSTRAINT fk_genre_id FOREIGN KEY (genre_id) REFERENCES genres (id) ON DELETE CASCADE,
-    CONSTRAINT fk_category_id FOREIGN KEY (category_id) REFERENCES category (id) ON DELETE CASCADE
+    CONSTRAINT fk_category_id FOREIGN KEY (category_id) REFERENCES categories (id) ON DELETE CASCADE
 );
