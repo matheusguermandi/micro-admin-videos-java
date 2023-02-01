e6de65aa60d2acb21610186ed7b71b66838b794b
matheusguermandi <matheus_guermandi@hotmail.com>
2023-02-01 21:30:57 +0000

    chore: update AudioVideoMedia test

--- domain/src/test/java/com/fullcycle/admin/catalogo/domain/video/AudioVideoMediaTest.java
+++ domain/src/test/java/com/fullcycle/admin/catalogo/domain/video/AudioVideoMediaTest.java
@@ -1,5 +1,6 @@
 package com.fullcycle.admin.catalogo.domain.video;
 
+import com.fullcycle.admin.catalogo.domain.utils.IdUtils;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -8,6 +9,7 @@ class AudioVideoMediaTest {
     @Test
     public void givenValidParams_whenCallsNewAudioVideo_ShouldReturnInstance() {
         // given
+        final var expectedId = IdUtils.uuid();
         final var expectedChecksum = "abc";
         final var expectedName = "Banner.png";
         final var expectedRawLocation = "/images/ac";
@@ -15,10 +17,12 @@ class AudioVideoMediaTest {
         final var expectedStatus = MediaStatus.PENDING;
 
         // when
-        final var actualVideo = AudioVideoMedia.with(expectedChecksum, expectedName, expectedRawLocation, expectedEncodedLocation, expectedStatus);
+        final var actualVideo =
+                AudioVideoMedia.with(expectedId, expectedChecksum, expectedName, expectedRawLocation, expectedEncodedLocation, expectedStatus);
 
         // then
         Assertions.assertNotNull(actualVideo);
+        Assertions.assertEquals(expectedId, actualVideo.id());
         Assertions.assertEquals(expectedChecksum, actualVideo.checksum());
         Assertions.assertEquals(expectedName, actualVideo.name());
         Assertions.assertEquals(expectedRawLocation, actualVideo.rawLocation());
@@ -32,9 +36,11 @@ class AudioVideoMediaTest {
         final var expectedChecksum = "abc";
         final var expectedRawLocation = "/images/ac";
 
-        final var img1 = AudioVideoMedia.with(expectedChecksum, "Random", expectedRawLocation, "", MediaStatus.PENDING);
+        final var img1 =
+                AudioVideoMedia.with(expectedChecksum, "Random", expectedRawLocation);
 
-        final var img2 = AudioVideoMedia.with(expectedChecksum, "Simple", expectedRawLocation, "", MediaStatus.PENDING);
+        final var img2 =
+                AudioVideoMedia.with(expectedChecksum, "Simple", expectedRawLocation);
 
         // then
         Assertions.assertEquals(img1, img2);
@@ -43,14 +49,29 @@ class AudioVideoMediaTest {
 
     @Test
     public void givenInvalidParams_whenCallsWith_ShouldReturnError() {
-        Assertions.assertThrows(NullPointerException.class, () -> AudioVideoMedia.with(null, "Random", "/videos", "/videos", MediaStatus.PENDING));
+        Assertions.assertThrows(
+                NullPointerException.class,
+                () -> AudioVideoMedia.with(null, "131", "Random", "/videos", "/videos", MediaStatus.PENDING)
+        );
 
-        Assertions.assertThrows(NullPointerException.class, () -> AudioVideoMedia.with("abc", null, "/videos", "/videos", MediaStatus.PENDING));
+        Assertions.assertThrows(
+                NullPointerException.class,
+                () -> AudioVideoMedia.with("id", "abc", null, "/videos", "/videos", MediaStatus.PENDING)
+        );
 
-        Assertions.assertThrows(NullPointerException.class, () -> AudioVideoMedia.with("abc", "Random", null, "/videos", MediaStatus.PENDING));
+        Assertions.assertThrows(
+                NullPointerException.class,
+                () -> AudioVideoMedia.with("id", "abc", "Random", null, "/videos", MediaStatus.PENDING)
+        );
 
-        Assertions.assertThrows(NullPointerException.class, () -> AudioVideoMedia.with("abc", "Random", "/videos", null, MediaStatus.PENDING));
+        Assertions.assertThrows(
+                NullPointerException.class,
+                () -> AudioVideoMedia.with("id", "abc", "Random", "/videos", null, MediaStatus.PENDING)
+        );
 
-        Assertions.assertThrows(NullPointerException.class, () -> AudioVideoMedia.with("abc", "Random", "/videos", "/videos", null));
+        Assertions.assertThrows(
+                NullPointerException.class,
+                () -> AudioVideoMedia.with("id", "abc", "Random", "/videos", "/videos", null)
+        );
     }
 }
