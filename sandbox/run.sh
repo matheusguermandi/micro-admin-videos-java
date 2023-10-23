f697cced81e2de08f973460030e566f546757d9f
matheusguermandi <matheus_guermandi@hotmail.com>
2023-10-23 20:27:00 +0100

    chore: add shell-script to run docker-compose and start all the containers

---
+++ sandbox/run.sh
@@ -0,0 +1,8 @@
+# Criar as pastas com permissões
+mkdir -m 777 .docker
+mkdir -m 777 .docker/keycloak
+
+docker compose -f app/docker-compose.yml up -d
+
+echo "Inicializando os containers..."
+sleep 20
